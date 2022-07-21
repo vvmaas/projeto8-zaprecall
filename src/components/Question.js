364c1d73144d54d28e8e5dd90cca6990ed4a808a
Victor Vermaas <vhvermaas@gmail.com>
2022-07-21 16:21:12 -0300

    flashcard cicle, bottombar integration, card shuffle

---
+++ src/components/Question.js
@@ -0,0 +1,62 @@
+import React from "react"
+
+export default function Question({question, answer, index, questionsDone, iconsBottom, wrongAnswer}){
+
+    let [undone, setUndone] = React.useState("question-undone")
+    let [open, setOpen] = React.useState("hidden")
+    let [answerState, setAnswerState] = React.useState("hidden")
+    let [done, setDone] = React.useState("hidden")
+    let [color, setColor] = React.useState("")
+    let [icon, setIcon] = React.useState("")
+
+    return (<>
+        <div class="question-undone" className={undone}>
+            <span> Pergunta {index}</span>
+            <ion-icon name="play-outline" onClick={() => {setUndone("hidden"); setOpen('question-open'); questionsDone()}}></ion-icon>
+        </div>
+        <div class="question-open" className={open}>
+            <br/>
+            <span>{question}</span>
+            <ion-icon name="git-compare-outline" onClick={() => {setOpen("hidden"); setAnswerState('question-answer')}}></ion-icon>
+        </div>
+        <div className={answerState}>
+            <span>{answer}</span>
+            <div className="question-result">
+
+                <div className="red-answer" onClick={() => {
+                    wrongAnswer();
+                    setAnswerState("hidden"); 
+                    setDone('question-done');
+                    setColor('red');
+                    setIcon("close-circle");
+                    iconsBottom("close-circle")
+                }}
+                    >Não lembrei</div>
+
+                <div className="orange-answer" onClick={() => {
+                    setAnswerState("hidden");
+                    setDone('question-done');
+                    setColor("orange");
+                    setIcon("help-circle");
+                    iconsBottom("help-circle")
+                    
+                }}
+                    >Quase não lembrei</div>
+
+                <div className="green-answer" onClick={() => {
+                    setAnswerState("hidden"); 
+                    setDone('question-done');
+                    setColor("green");
+                    setIcon("checkmark-circle");
+                    iconsBottom("checkmark-circle")
+                    
+                }}
+                    >Zap!</div>
+            </div>
+        </div>
+        <div class="question-done" className={done}>
+            <span className={color}>Pergunta {index}</span>
+            <ion-icon name={icon}></ion-icon>
+        </div>
+   </> )
+}
--- src/components/RunPage.js
+++ src/components/RunPage.js
@@ -1,47 +1,109 @@
-function Question(){
-    return (<>
-        <div className="question-undone">
-            <span>Pergunta X</span>
-            <ion-icon></ion-icon>
-        </div>
-        <div className="question-open">
-            <br/>
-            <span>O que é o que é?</span>
-            <ion-icon></ion-icon>
-        </div>
-        <div className="question-answer">
-            <span>é o que é.</span>
-            <div className="question-result">
-                <div className="red-answer">Não lembrei</div>
-                <div className="orange-answer">Quase não lembrei</div>
-                <div className="green-answer">Zop!</div>
-            </div>
-        </div>
-        <div className="question-done">
-            <span>Pergunta X</span>
-            <ion-icon></ion-icon>
-        </div>
-   </> )
-}
+import React from "react"
+import Question from "./Question"
+
 
 export default function RunPage() {
+
+    const [wrong, setWrong] = React.useState(0);
+    const [qtd, setQtd] = React.useState(0);
+    const [icon1, setIcon1] = React.useState("");
+    const [icon2, setIcon2] = React.useState("");
+    const [icon3, setIcon3] = React.useState("");
+    const [icon4, setIcon4] = React.useState("");
+    let [msgBom, setMsgBom] = React.useState("hidden")
+    let [msgRuim, setMsgRuim] = React.useState("hidden")
+
+    function checkWrong(ans){
+        if(wrong > 0 || ans === "close-circle"){
+            setMsgRuim("end-message")
+        } else {
+            setMsgBom("end-message")
+        }
+    }
+
+    function iconsBottom(ans) {
+        if(icon1===""){
+            setIcon1(ans)
+        } else if (icon2===""){
+            setIcon2(ans)
+        } else if (icon3===""){
+            setIcon3(ans)
+        } else if (icon4===""){
+            checkWrong(ans)
+            setIcon4(ans)
+        }
+    }
+
+    function wrongAnswer() {
+		setWrong(wrong+1);
+	}
+
+	function questionsDone() {
+		setQtd(qtd+1);
+	}
+
+    let questionsObj=[
+        {question: 'O que é JSX?', answer: 'Uma extensão de linguagem do JavaScript'},
+        {question: 'O React é __ ', answer: 'uma biblioteca JavaScript para construção de interfaces'},
+        {question: 'Componentes devem iniciar com __?', answer: 'letra maiúscula'},
+        {question: 'Podemos colocar __ dentro do JSX', answer: 'expressões'},
+        {question: 'O ReactDOM nos ajuda __ ', answer: 'interagindo com a DOM para colocar componentes React na mesma'},
+        {question: 'Usamos o npm para __', answer: 'gerenciar os pacotes necessários e suas dependências'},
+        {question: 'Usamos props para __', answer: 'passar diferentes informações para componentes '},
+        {question: 'Qual a resposta para a Vida, o Universo e Tudo o Mais?', answer: '42'}]
+    
+    let questionsRun=[]
+
+    function randomizer(){
+        return Math.random() - 0.5
+    }
+
+    function shuffle(){
+        questionsObj.sort(randomizer)
+        for (let i = 0; i<4; i++){
+            questionsRun.push(questionsObj[i])
+        }
+    }
+    shuffle()
+
     return (
         <div className="run-page">
-            <div className="logo-run">
+            <div className="logo-run" onClick={reload}>
                 <img src="assets/zap.png" alt=""/>
                 <h1>ZapRecall</h1>
             </div>
             <div className="questions">
-                <Question />
+                {questionsRun.map((qObj, index) => (<Question index={index + 1}
+                                                                     question={qObj.question} 
+                                                                     answer={qObj.answer} 
+                                                                     qtd={qtd}
+                                                                     questionsDone={questionsDone}
+                                                                     iconsBottom={iconsBottom}
+                                                                     wrongAnswer={wrongAnswer}/>))}
+                                                                   
             </div>
             <div className="progress-bar">
-                <div className="end-message hidden">
+                <div class="end-message" className={msgBom}>
                     <strong>Parabéns!</strong>
                     <span>Você não esqueceu nenhum flashcard!</span>
                 </div>
-                <div className="progress-count">x/4 concluidos</div>
-                <div className="progress-icons"></div>
+                <div class="end-message" className={msgRuim}>
+                    <strong>Putz...</strong>
+                    <span>Ainda faltam alguns...</span>
+                    <span>Mas não desista!</span>
+                </div>
+                <div className="progress-count">{qtd}/4 concluidos</div>
+                <div className="progress-icons">
+                    <ion-icon name={icon1}></ion-icon>
+                    <ion-icon name={icon2}></ion-icon>
+                    <ion-icon name={icon3}></ion-icon>
+                    <ion-icon name={icon4}></ion-icon>
+                </div>
             </div>
         </div>
     )
+}
+
+function reload(){
+    window.location.reload()
 }
