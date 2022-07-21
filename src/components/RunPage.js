7e2cec2606526f97e2ce46cda8c4982bcc475898
Victor Vermaas <vhvermaas@gmail.com>
2022-07-21 19:32:42 -0300

    shuffle bug fix

--- src/components/RunPage.js
+++ src/components/RunPage.js
@@ -2,7 +2,7 @@ import React from "react"
 import Question from "./Question"
 
 
-export default function RunPage() {
+export default function RunPage({questionsRun}) {
 
     const [wrong, setWrong] = React.useState(0);
     const [qtd, setQtd] = React.useState(0);
@@ -42,30 +42,6 @@ export default function RunPage() {
 		setQtd(qtd+1);
 	}
 
-    let questionsObj=[
-        {question: 'O que é JSX?', answer: 'Uma extensão de linguagem do JavaScript'},
-        {question: 'O React é __ ', answer: 'uma biblioteca JavaScript para construção de interfaces'},
-        {question: 'Componentes devem iniciar com __?', answer: 'letra maiúscula'},
-        {question: 'Podemos colocar __ dentro do JSX', answer: 'expressões'},
-        {question: 'O ReactDOM nos ajuda __ ', answer: 'interagindo com a DOM para colocar componentes React na mesma'},
-        {question: 'Usamos o npm para __', answer: 'gerenciar os pacotes necessários e suas dependências'},
-        {question: 'Usamos props para __', answer: 'passar diferentes informações para componentes '},
-        {question: 'Qual a resposta para a Vida, o Universo e Tudo o Mais?', answer: '42'}]
-    
-    let questionsRun=[]
-
-    function randomizer(){
-        return Math.random() - 0.5
-    }
-
-    function shuffle(){
-        questionsObj.sort(randomizer)
-        for (let i = 0; i<4; i++){
-            questionsRun.push(questionsObj[i])
-        }
-    }
-    shuffle()
-
     return (
         <div className="run-page">
             <div className="logo-run" onClick={reload}>
@@ -79,7 +55,8 @@ export default function RunPage() {
                                                                      qtd={qtd}
                                                                      questionsDone={questionsDone}
                                                                      iconsBottom={iconsBottom}
-                                                                     wrongAnswer={wrongAnswer}/>))}
+                                                                     wrongAnswer={wrongAnswer}
+                                                                    />))}
                                                                    
             </div>
             <div className="progress-bar">
